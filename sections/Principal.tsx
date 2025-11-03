19797f6eadd6171038b8cdf975234b7a80824cf5
Apestey Santiago <apesteysantiago@hotmail.com>
2025-11-03 16:19:51 -0300

    frase

--- sections/Principal.tsx
+++ sections/Principal.tsx
@@ -8,7 +8,7 @@ export default function Principal() {
       initial={{ opacity: 0, x: -100 }}   // 100% opacidad, fuera de la izquierda
       animate={{ opacity: 1, x: 0 }}      // 0% opacidad, llega al centro
       transition={{ duration: 1.5, ease: "easeOut" }} // tiempo y suavizado
-      className="flex flex-col  rounded-lg items-center justify-center"
+      className="flex flex-col rounded-lg items-center justify-center"
       >
         <h1 id="titulo" className="flex justify-center ml-6">TEAM FAKAS</h1>
         <h2 id="subtitulo" className="flex justify-center ml-10">PERSONAL TRAINERS</h2>
@@ -16,10 +16,18 @@ export default function Principal() {
         - Planes Personalizados - Nutrición - Seguimiento - Coaching Psicologico -
         </h3>
       </motion.div>
-      
-      <div className="" ></div>
-      
-      
+
+      <motion.div
+      initial={{ opacity: 0, x: -100 }}   // 100% opacidad, fuera de la izquierda
+      animate={{ opacity: 1, x: 0 }}      // 0% opacidad, llega al centro
+      transition={{ duration: 1.5, ease: "easeOut" }} // tiempo y suavizado
+      className="flex flex-col items-center justify-center h-screen"
+      >
+        <h2 id="frase" className="text-center text-2xl">¿Estás listo para tu mejor versión?</h2>
+
+      </motion.div>
+
+
     </section>
   );
 }
