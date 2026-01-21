4e6684dd3542ea90a99176c34f2fbb1dd9a9020e
Apestey Santiago <apesteysantiago@hotmail.com>
2026-01-21 15:54:41 -0300

    DIAPO NUEVA

--- sections/Seccion6.tsx
+++ sections/Seccion6.tsx
@@ -4,13 +4,15 @@ export default function Section6() {
             <div
                 className="flex flex-col rounded-lg items-center justify-center"
             >
-                <h1 id="titulo6" className="flex justify-center text-center font-2xl">¡La Pelea Que Nadie Ve!</h1>
-                <h2 id="subtitulo6" className="flex justify-center pt-2 ">¡TE ESPERAMOS PRONTO!</h2>
+                <h1 id="titulo6" className="-translate-y-40 flex justify-center text-center font-2xl">LA IA NO TRANSPIRA</h1>
+                <h2 id="subtitulo6" className="-translate-y-40 flex justify-center pt-2 ">TU PROGRESO NO ES UN ALGORITMO</h2>
 
-                <div id="descripcion" className="flex bg-white/10 backdrop-blur-sm border border-white/20 rounded-xl shadow-lg text-center w-fit max-w-[90%] mx-auto">
-                    <p className="text-center p-4">Todos tenemos una voz que nos empuja a mejorar… y otra que intenta frenarnos.<br></br><br></br>
-                        Esa batalla interna decide si avanzamos, si retrocedemos o si seguimos estancados en el mismo cuerpo que queremos cambiar.<br></br><br></br>
-                        Acá aprendés a dominar tu mente, a ordenar tus hábitos y a convertir el esfuerzo en resultados visibles.<br></br></p>
+                <div id="descripcion" className="translate-y-20 flex bg-white/10 backdrop-blur-sm border border-white/20 rounded-xl shadow-lg text-center w-fit max-w-[90%] mx-auto">
+                    <p className="text-center p-4">La IA es ciega ante tu cansancio, tus miedos o ese fuego que te empuja a seguir cuando el cuerpo ya no responde. Una IA te tira una receta vacía; nosotros te damos el camino que necesitás.<br></br><br></br>
+
+                    Confiar tu salud a una aplicación es quedarte solo en el momento más difícil. En Teamfakas, entendemos que detrás de cada entrenamiento hay una historia, una lucha y una persona real que merece ser escuchada, no procesada.
+
+                    </p>
                 </div>
             </div>
         </section>
